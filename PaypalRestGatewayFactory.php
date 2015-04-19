29fdaa648135bab2faac97688d96696bbaea5a77
Ningsuhen Waikhom <ningsuhen@gmail.com>
2015-04-19 18:04:13 +0530

    Return ApiContext
    Return ApiContext - this returned value is added to the payment apis list.

--- PaypalRestGatewayFactory.php
+++ PaypalRestGatewayFactory.php
@@ -79,6 +79,7 @@ class PaypalRestGatewayFactory implements GatewayFactoryInterface
 
                 $credential = new OAuthTokenCredential($config['client_id'], $config['client_secret']);
                 $config['payum.api'] = new ApiContext($credential);
+                return $config['payum.api'];
             };
         }
 
